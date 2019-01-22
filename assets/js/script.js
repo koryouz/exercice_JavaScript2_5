42e5f97a217eff276888cc7c81b1072a4fc63189
DELHAY <delhayantoine@gmail.com>
2019-01-22 08:35:24 +0100

    Ajout de l'exercice 5

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("myImg1").src = "assets/images/image1_2.jpg";
+  document.getElementById("myImg2").src = "assets/images/image2_2.jpg";
+  document.getElementById("myImg3").src = "assets/images/image3_2.jpg";
+  document.getElementById("myImg4").src = "assets/images/image4_2.jpg";
+  document.getElementById("myImg5").src = "assets/images/image5_2.jpg";
+}
