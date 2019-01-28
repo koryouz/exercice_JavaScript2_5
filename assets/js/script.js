ed8d47af20e6bc22bbe84515802765bfb00e349a
DELHAY <delhayantoine@gmail.com>
2019-01-28 14:17:23 +0100

    Update du JS, update image by image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
-function myFunction() {
-  document.getElementById("myImg1").src = "assets/images/image1_2.jpg";
-  document.getElementById("myImg2").src = "assets/images/image2_2.jpg";
-  document.getElementById("myImg3").src = "assets/images/image3_2.jpg";
-  document.getElementById("myImg4").src = "assets/images/image4_2.jpg";
-  document.getElementById("myImg5").src = "assets/images/image5_2.jpg";
+// Déclaration de ma fonction changeImg().
+function changeImg(object,ev){
+  //On récupère les images et on les assigne a une variable.
+  var img1 = document.getElementById('image1');
+  var img2 = document.getElementById('image2');
+  var img3 = document.getElementById('image3');
+  var img4 = document.getElementById('image4');
+  var img5 = document.getElementById('image5');
+  //On assigne un nouvel src à l'image sous la souris
+  if(ev == 'img1'){
+    img1.setAttribute('src', 'assets/images/image1_2.jpg');
+  }
+  if(ev == 'img2'){
+    img2.setAttribute('src', 'assets/images/image2_2.jpg');
+  }
+  if(ev == 'img3'){
+    img3.setAttribute('src', 'assets/images/image3_2.jpg');
+  }
+  if(ev == 'img4'){
+    img4.setAttribute('src', 'assets/images/image4_2.jpg');
+  }
+  if(ev == 'img5'){
+    img5.setAttribute('src', 'assets/images/image5_2.jpg');
+  }
 }
